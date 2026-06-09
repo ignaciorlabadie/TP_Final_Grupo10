5d2879189c14bfbe3801e01dfb93839719d03f43
Ignacio <ignacioramirezlabadie@gmail.com>
2026-06-09 13:31:55 -0300

    refactor: adecuar conexion a base de datos en base al ejemplo

---
+++ backend/core/server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const errorHandler = require('../middleware/error-handler.middleware')
+const { sequelize } = require('../models')
+const { establecerCardinalidad } = require('../models/cardinalidades.model')
+
+class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT || 3000
+    this.connectToDataBase()
+    this.middleware()
+    this.rutas()
+    this.errorHandlerGlobal()
+  }
+
+  middleware() {
+    this.app.use(cors())
+    this.app.use(express.json())
+  }
+
+  rutas() {
+    this.app.use('/ejercicios', require('../routes/ejercicio.js'))
+    this.app.use('/rutinas', require('../routes/rutina.js'))
+    this.app.use('/entrenamientos', require('../routes/entrenamiento.js'))
+    this.app.use('/estadisticas', require('../routes/estadisticas.js'))
+    // this.app.use('/auth', require('../routes/auth.js'))
+  }
+
+  async connectToDataBase() {
+    try {
+      establecerCardinalidad()
+      console.log(
+        'Cardinalidad y relaciones entre tablas establecidas correctamente'
+      )
+
+      await sequelize.sync({ alter: false })
+      console.log('Database sincronizada correctamente')
+    } catch (error) {
+      console.error('Error en la conexión a la DB: ', error)
+    }
+  }
+
+  errorHandlerGlobal() {
+    this.app.use((err, req, res, next) => {
+      console.error(err.stack)
+      return res.status(404).json({ msg: 'Error. Pagina no encontrada' })
+    })
+    this.app.use(errorHandler)
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`La API esta escuchando el el puerto: ${this.port}`)
+    })
+  }
+}
+
+module.exports = Server
