08666f4af4871d09f5efb84a74382ace98b5dad5
EmmanuelF90 <emmanuel_22.08@hotmail.com>
2026-06-11 19:50:50 -0300

    arreglos a entrenamiento model

--- backend/models/entrenamiento.model.ts
+++ backend/models/entrenamiento.model.ts
@@ -1,12 +1,8 @@
 import { sequelize } from './index'
-import { DataTypes, Model } from 'sequelize'
-import {
-  InterfaceEntrenamiento
-} from './interfaces/entrenamiento.interface'
+import { DataTypes, Model, Optional } from 'sequelize'
+import { InterfaceEntrenamiento } from './interfaces/entrenamiento.interface'
 
-interface InputEntrenamiento extends Omit<InterfaceEntrenamiento, 'id' | 'fecha'> {}
-interface EntrenamientoUpdateData extends Partial<InputEntrenamiento> {}
-interface EntrenamientoCreationAttributes extends InputEntrenamiento {}
+interface EntrenamientoCreationAttributes extends Optional<InterfaceEntrenamiento, 'id' | 'fecha'> {}
 
 export class EntrenamientoModel
   extends Model<InterfaceEntrenamiento, EntrenamientoCreationAttributes>
@@ -20,19 +16,19 @@ export class EntrenamientoModel
   declare readonly createdAt: Date
   declare readonly updatedAt: Date
 
-  static async findAllEntrenamientos(options: any): Promise<EntrenamientoModel[]> {
-    return await EntrenamientoModel.findAll(options)
+  static async findAllEntrenamientos(): Promise<EntrenamientoModel[]> {
+    return await EntrenamientoModel.findAll()
   }
 
-  static async findById(id: number, options: any): Promise<EntrenamientoModel | null> {
-    return await EntrenamientoModel.findByPk(id, options)
+  static async findById(id: number): Promise<EntrenamientoModel | null> {
+    return await EntrenamientoModel.findByPk(id)
   }
 
-  static async createEntrenamiento(input: InputEntrenamiento): Promise<EntrenamientoModel> {
+  static async createEntrenamiento(input: EntrenamientoCreationAttributes): Promise<EntrenamientoModel> {
     return await EntrenamientoModel.create(input)
   }
 
-  static async updateEntrenamiento(id: number, data: EntrenamientoUpdateData): Promise<EntrenamientoModel | null> {
+  static async updateEntrenamiento(id: number, data: Partial<EntrenamientoCreationAttributes>): Promise<EntrenamientoModel | null> {
     const entrenamiento = await this.findByPk(id)
     if (!entrenamiento) return null
     return await entrenamiento.update(data)
