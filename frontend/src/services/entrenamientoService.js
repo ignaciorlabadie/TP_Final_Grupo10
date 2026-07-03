803f02a26918e97497c889e0af658bba72b5efc5
Ignacio Ramirez Labadie <ignacioramirezlabadie@gmail.com>
2026-07-02 22:18:27 -0300

    Revert "feat: configuracion inicial y servicios"

--- frontend/src/services/entrenamientoService.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const listarEntrenamientos = () => api.get('/entrenamientos');
-
-export const getEntrenamiento = (id) => api.get(`/entrenamientos/${id}`);
-
-export const crearEntrenamiento = (data) => api.post('/entrenamientos', data);
-
-export const eliminarEntrenamiento = (id) => api.delete(`/entrenamientos/${id}`);
